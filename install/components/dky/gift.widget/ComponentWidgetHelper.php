0f0cb74607079fbcd3f1304af10da4072d1198ee
dimabresky <dimabresky@gmail.com>
2021-03-10 14:46:54 +0300

    fix: coupon calculation for basket

--- install/components/dky/gift.widget/ComponentWidgetHelper.php
+++ install/components/dky/gift.widget/ComponentWidgetHelper.php
@@ -67,12 +67,10 @@ class ComponentWidgetHelper extends Sale\BasketComponentHelper {
         $oldApiStatus = Sale\Compatible\DiscountCompatibility::isUsed(); // TODO: remove this code after refactoring DiscountCompatibility
         if ($oldApiStatus)
             Sale\Compatible\DiscountCompatibility::stopUsageCompatible();
-        //DiscountCouponsManager::freezeCouponStorage();
         $basket->refreshData(array('PRICE', 'COUPONS'));
         $discounts = Sale\Discount::buildFromBasket($basket, new Sale\Discount\Context\Fuser($basket->getFUserId(true)));
         $discounts->calculate();
         $discountResult = $discounts->getApplyResult();
-        //DiscountCouponsManager::unFreezeCouponStorage();
         if ($oldApiStatus)
             Sale\Compatible\DiscountCompatibility::revertUsageCompatible();
 
